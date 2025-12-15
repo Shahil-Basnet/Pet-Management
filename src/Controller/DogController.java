222e32ceb2fbecacda8987367fcc604223a5b8c4
Shahil Basnet <shahilbasnet434@gmail.com>
2025-12-15 22:08:35 +0545

    Add methods for CRUD operations in controller

--- src/Controller/DogController.java
+++ src/Controller/DogController.java
@@ -3,11 +3,32 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Controller;
-
+import Model.Dog;
 /**
  *
  * @author Shahil
  */
 public class DogController {
+    /**
+     * Creates "Dog" object
+     * 
+     * @param dogList arrayList? of DogModel
+     * @return
+     * @throws
+     */
+    public void addDog(){
+        
+    }
     
+    public void deleteDog(){
+        
+    }
+    
+    public void updateDog(){
+        
+    }
+    
+    public void validateDogs(){
+        
+    }
 }
